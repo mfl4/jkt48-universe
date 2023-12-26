554ead43a0e08057ec53f3ea1a03b3b9d13a2664
mfl4 <mhdfarhanl6267@gmail.com>
2023-12-27 01:57:58 +0700

    [PUSH] refac: add a route for home page

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,12 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "web" middleware group. Make something great!
 |
-*/
+ */
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/home', function () {
+    return view('pages.home');
 });
