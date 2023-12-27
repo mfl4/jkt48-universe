8a46213b7ffb699343c6fe322ab749b74973b02e
mfl4 <mhdfarhanl6267@gmail.com>
2023-12-27 17:01:49 +0700

    [PUSH] refac: add new route for Member CRUD in Dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DashboardMemberController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,3 +21,5 @@ Route::get('/', function () {
 Route::get('/home', function () {
     return view('pages.home');
 });
+
+Route::resource('/dashboard/members', DashboardMemberController::class);
