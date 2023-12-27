0795618dc8b4e11d0bcfb4a28d4d31880cd0a2d9
mfl4 <mhdfarhanl6267@gmail.com>
2023-12-27 20:17:02 +0700

    [PUSH] feat: create model & main controller for members

---
+++ app/Http/Controllers/MemberController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\Member;
+
+class MemberController extends Controller
+{
+    public function index()
+    {
+
+    }
+
+    public function show(Member $member)
+    {
+
+    }
+}
