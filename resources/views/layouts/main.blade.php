616bc810bea327a27040b00b93438abcddebb535
mfl4 <mhdfarhanl6267@gmail.com>
2023-12-28 23:59:26 +0700

    [PUSH] refac: change the layout structure

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,45 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>JKT48 Universe</title>
-    <script src="https://cdn.tailwindcss.com"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
-        integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link
-        href="https://fonts.googleapis.com/css2?family=Arvo:wght@400;700&family=Lato:wght@300;400;700&family=Yellowtail&display=swap"
-        rel="stylesheet">
-    <style>
-        * {
-            font-family: 'Lato',
-                sans-serif;
-            margin: 0;
-            padding: 0;
-            box-sizing: border-box;
-            scroll-behavior: smooth;
-        }
-
-        .bg-font {
-            font-family: 'Yellowtail',
-                cursive;
-        }
-
-        /* font-family: 'Arvo',
-        serif; */
-    </style>
-
-</head>
-
-<body>
+@extends('layouts.app')
+@section('layout')
+    @include('partials.navbar')
     <main>
         @yield('content')
     </main>
-</body>
-
-</html>
+    @include('partials.footer')
+@endsection
