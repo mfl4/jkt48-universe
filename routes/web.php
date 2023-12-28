2be495b2274997beeafdfdaf0759a762b9301dc3
mfl4 <mhdfarhanl6267@gmail.com>
2023-12-29 00:03:16 +0700

    [PUSH] refac: add routes for authentication & dashboard member

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DashboardMemberController;
+use App\Http\Controllers\MemberController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,12 +16,37 @@ use Illuminate\Support\Facades\Route;
 |
  */
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages.home');
 });
 
-Route::get('/home', function () {
-    return view('pages.home');
+Route::get('/members', function () {
+    return view('pages.members');
+});
+
+Route::get('/members', [MemberController::class, 'index']);
+
+Route::get('/members/{post:id}', [MemberController::class, 'show']);
+
+Route::get('/signin', [AuthController::class, 'signin'])->name('signin')->middleware('guest');
+Route::post('/signin', [AuthController::class, 'authenticate']);
+Route::post('/signout', [AuthController::class, 'signout']);
+
+Route::get('/signup', [AuthController::class, 'signup'])->name('signup')->middleware('guest');
+Route::post('/signup', [AuthController::class, 'store']);
+
+Route::get('/signin', function () {
+    return view('pages.auth.signin');
+});
+
+Route::get('/dashboard', function () {
+    return view('pages.dashboard.index', [
+        'title' => 'Dashboard',
+    ]);
 });
 
 Route::resource('/dashboard/members', DashboardMemberController::class);
