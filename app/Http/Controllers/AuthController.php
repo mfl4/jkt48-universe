90782eb29077411c7c8f119bf9e1df6112fae48a
mfl4 <mhdfarhanl6267@gmail.com>
2024-01-02 00:13:17 +0700

    [PUSH] refac: add method for store data while signup

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,11 +3,28 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
     public function signup()
     {
         return view('pages.auth.signup');
+    }
+
+    public function store()
+    {
+        $validatedData = request()->validate([
+            'name' => 'required|max:255',
+            'username' => 'required|min:3|max:255|unique:users,username',
+            'email' => 'required|email|max:255|unique:users,email',
+            'password' => 'required|min:3|max:255',
+        ]);
+
+        $validatedData['password'] = Hash::make($validatedData['password']);
+
+        User::create($validatedData);
+        return redirect()->route('auth.signin')->with('success', 'You have successfully signed up, please sign in!');
     }
 }
