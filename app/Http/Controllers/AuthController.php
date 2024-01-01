d640bcdbf0ed29eace2e7e158b281ac32f319372
mfl4 <mhdfarhanl6267@gmail.com>
2024-01-01 23:57:11 +0700

    [PUSH] feat: add method for signin

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+    public function signin()
+    {
+        return view('pages.auth.signin');
+    }
 }
