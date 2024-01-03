07585a9077ae19be44ae3d554f1669519a52d5da
mfl4 <mhdfarhanl6267@gmail.com>
2024-01-03 16:43:40 +0700

    [PUSH] refac: make routes more clean

--- routes/web.php
+++ routes/web.php
@@ -19,19 +19,19 @@ use Illuminate\Support\Facades\Route;
 // Home
 Route::get('/', function () {
     return view('pages.home');
-});
+})->name('home');
 
 // Members
 Route::prefix('members')->group(function () {
-    Route::get('/', [MemberController::class, 'index']);
-    Route::get('/{post:id}', [MemberController::class, 'show']);
+    Route::get('/', [MemberController::class, 'index'])->name('members');
+    Route::get('/{member:id}', [MemberController::class, 'show'])->name('member');
 });
 
 // Authentication
 Route::group(['prefix' => 'auth'], function () {
-    Route::get('/signup', [AuthController::class, 'signup'])->name('signup')->middleware('guest');
+    Route::get('/signup', [AuthController::class, 'signup'])->name('auth.signup')->middleware('guest');
     Route::post('/signup', [AuthController::class, 'store']);
-    Route::get('/signin', [AuthController::class, 'signin'])->name('signin')->middleware('guest');
+    Route::get('/signin', [AuthController::class, 'signin'])->name('auth.signin')->middleware('guest');
     Route::post('/signin', [AuthController::class, 'authenticate']);
     Route::post('/signout', [AuthController::class, 'signout']);
 });
@@ -40,7 +40,9 @@ Route::group(['prefix' => 'auth'], function () {
 Route::get('/dashboard', function () {
     return view('pages.dashboard.index', [
         'title' => 'Dashboard',
+        'users' => \App\Models\User::all()->count(),
+        'members' => \App\Models\Member::all()->count(),
     ]);
-})->middleware('auth')->name('dashboard');
+})->middleware('auth')->name('dashboard.index');
 
 Route::resource('/dashboard/members', DashboardMemberController::class);
