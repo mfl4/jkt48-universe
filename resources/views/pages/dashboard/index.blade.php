855cb405dc8f90f485df904db484a2cfe1b8aac1
mfl4 <mhdfarhanl6267@gmail.com>
2024-01-03 20:53:32 +0700

    [PUSH] refac: get data & display on dashboard view

--- resources/views/pages/dashboard/index.blade.php
+++ resources/views/pages/dashboard/index.blade.php
@@ -1,4 +1,8 @@
 @extends('layouts.dashboard')
 @section('content')
-    <h1>Dashboard</h1>
+    <h1 class="font-black bg-red-700 text-white p-3 rounded">Welcome to Dashboard, {{ auth()->user()->name }}</h1>
+    <hr class="my-4">
+
+    <h2>Users : {{ $users }}</h2>
+    <h2>Members : {{ $members }}</h2>
 @endsection
