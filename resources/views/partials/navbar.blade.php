a4660591324e6808a4771d13b3fdcc327b0b2f35
mfl4 <mhdfarhanl6267@gmail.com>
2024-01-04 16:11:18 +0700

    [PUSH] fix: make navbar more interesting and clean

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,33 +1,42 @@
-<nav class="sticky top-0 bg-red-700 text-white z-50 p-3 flex justify-between items-center rounded-b-lg"
-    data-aos="zoom-in-down">
+<nav class="sticky top-0 bg-red-700 text-white z-50 p-3 flex justify-between items-center shadow-2xl shadow-red-700"
+    data-aos="fade-down">
     <section>
         <ul class="flex gap-5">
             <li>
-                <a href="/"
-                    class="rounded p-2 transition-all duration-300 hover:bg-red-800 {{ request()->is('/') ? ' font-extrabold bg-red-950' : '' }}">Home</a>
+                <a href="{{ Route('home') }}"
+                    class="rounded  p-2 transition-all duration-300 hover:bg-white hover:text-red-600 hover:font-bold {{ request()->is('/') ? 'text-red-700 font-extrabold rounded bg-white' : 'text-white' }}">Home</a>
             </li>
             <li>
-                <a href="/members"
-                    class="rounded p-2 transition-all duration-300 hover:bg-red-800 {{ request()->is('members') ? ' font-extrabold bg-red-950' : '' }}">Members</a>
+                <a href="{{ Route('members') }}"
+                    class="rounded  p-2 transition-all duration-300 hover:bg-white hover:text-red-600 hover:font-bold {{ request()->is('members*') ? 'text-red-700 font-extrabold rounded bg-white' : 'text-white' }}">Members</a>
             </li>
             <li>
-                <a href="/dashboard"
-                    class="rounded p-2 transition-all duration-300 hover:bg-red-800 {{ request()->is('dashboard') ? ' font-extrabold bg-red-950' : '' }}">Dashboard</a>
+                <a href="{{ Route('dashboard') }}"
+                    class="rounded  p-2 transition-all duration-300 hover:bg-white hover:text-red-600 hover:font-bold {{ request()->is('dashboard*') ? 'text-red-700 font-extrabold rounded bg-white' : 'text-white' }}">Dashboard</a>
             </li>
         </ul>
     </section>
     <section class="flex items-center gap-2">
-        <img class="mx-auto w-8" src="https://jkt48.com/images/logo.svg" alt="JKT48 Universe">
+        <img class="mx-auto w-8" src="{{ asset('images/jkt48.svg') }}" alt="JKT48 Universe">
         <h1 class="text-3xl font-black text-white ">JKT48 Universe</h1>
     </section>
 
     <section>
         <ul>
-            <li>
-                <a href="/signin"
-                    class="hover:bg-red-900 hover:text-white hover:font-bold rounded font-semibold bg-white text-red-700 p-2 transition-all duration-300 {{ request()->is('signin') ? ' font-extrabold' : '' }}">Sign
-                    In</a>
-            </li>
+            @auth
+                <li>
+                    <a class="rounded font-semibold bg-white text-red-700 p-2 transition-all duration-300" href="/"
+                        role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Welcome, {{ auth()->user()->name }}
+                    </a>
+                </li>
+            @else
+                <li>
+                    <a href="{{ Route('signin') }}"
+                        class="rounded font-semibold bg-white text-red-700 p-2 transition-all duration-300 hover:bg-red-900 hover:text-white hover:font-bold {{ request()->is('signin') ? ' font-extrabold' : '' }}">Sign
+                        In</a>
+                </li>
+            @endauth
         </ul>
     </section>
 
