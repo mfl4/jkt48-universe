888efd3a5c3f9babb36f93ac8ccb90078a1606ff
mfl4 <mhdfarhanl6267@gmail.com>
2024-01-04 16:11:50 +0700

    [PUSH] refac: make home page more interesting

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,5 +1,8 @@
 @extends('layouts.main')
 
 @section('content')
-    <h1>Home Page</h1>
+    <section class="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+        <h1 class="border-b border-red-700 p-4 text-2xl font-black tracking-tight text-red-700" data-aos="fade-down-left">
+            Hello Im under the water</h1>
+    </section>
 @endsection
