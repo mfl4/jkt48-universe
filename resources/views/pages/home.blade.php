86e26273c53381907fea4af437f70ae6e13a2cd9
mfl4 <mhdfarhanl6267@gmail.com>
2024-01-06 19:46:54 +0700

    [PUSH] style: make the home ui more clean

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,8 +1,6 @@
 @extends('layouts.main')
 
 @section('content')
-    <section class="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-        <h1 class="border-b border-red-700 p-4 text-2xl font-black tracking-tight text-red-700" data-aos="fade-down-left">
-            Hello Im under the water</h1>
-    </section>
+    <h1 class="border-b border-red-700 text-2xl font-black tracking-tight text-red-700" data-aos="fade-down-left">
+        Hello Im under the water</h1>
 @endsection
