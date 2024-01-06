659aa254b0f60ff4dec3adbdcf4088f09b581dba
mfl4 <mhdfarhanl6267@gmail.com>
2024-01-06 19:43:20 +0700

    [PUSH] style: redesign ui for layout & add animation wth aos

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -2,7 +2,7 @@
 @section('layout')
     <div class="flex">
         @include('partials.sidebar')
-        <section class="w-full p-5">
+        <section class="w-full p-5" data-aos="fade-left" data-aos-delay="50">
             @yield('content')
         </section>
     </div>
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 @section('layout')
     @include('partials.navbar')
-    <main>
+    <main class="mx-auto max-w-2xl px-4 py-4 sm:px-6 sm:py-12 lg:max-w-7xl lg:px-8">
         @yield('content')
     </main>
     @include('partials.footer')
