12dd139e75d4b5101181f8a51a591c021826b45f
mfl4 <mhdfarhanl6267@gmail.com>
2024-01-06 21:25:49 +0700

    [PUSH] refac: redesign member ui

--- resources/views/pages/member.blade.php
+++ resources/views/pages/member.blade.php
@@ -2,43 +2,58 @@
 
 @section('content')
     <section class="flex justify-between items-center">
-        <a href="{{ route('members') }}" class="font-black text-white bg-red-700 p-3 m-2 rounded text-center">Back
+        <a href="{{ route('members') }}" class="font-black text-white bg-red-700 p-3 m-2 rounded text-center"
+            data-aos="fade-left" data-aos-delay="50">Back
             to Members</a>
         <section>
             @if ($member->id <= 1)
-                <span class="text-red-700 font-black p-3 m-2 rounded">No Previous Member</span>
+                <span class="text-red-700 font-black p-3 m-2 rounded" data-aos="fade-right" data-aos-delay="100">No Previous
+                    Member</span>
             @else
                 <a href="{{ route('member', $member->id - 1) }}"
-                    class="font-black text-white bg-red-700 p-3 m-2 rounded text-center">Previous Member</a>
+                    class="font-black text-white bg-red-700 p-3 m-2 rounded text-center" data-aos="fade-right"
+                    data-aos-delay="100">Previous Member</a>
             @endif
 
             @if ($member->id >= $member->count())
-                <span class="text-red-700 font-black p-3 m-2 rounded">No Next Member</span>
+                <span class="text-red-700 font-black p-3 m-2 rounded" data-aos="fade-right" data-aos-delay="50">No Next
+                    Member</span>
             @else
                 <a href="{{ route('member', $member->id + 1) }}"
-                    class="font-black text-white bg-red-700 p-3 m-2 rounded text-center">Next Member</a>
+                    class="font-black text-white bg-red-700 p-3 m-2 rounded text-center" data-aos="fade-right"
+                    data-aos-delay="50">Next Member</a>
             @endif
         </section>
     </section>
-    <h1 class="border-b border-red-700 p-4 text-2xl font-black tracking-tight text-red-700" data-aos="fade-down-left">
-        {{ $member->name }}</h1>
-    <h1 class="text-9xl bg-font opacity-80 text-center items-center -rotate-12 absolute right-52 bottom-52 animate-pulse"
-        data-aos="zoom-in-left">
-        {{ $member->name }}
-    </h1>
-    @if ($member->photo)
-        <img class="absolute right-52 bottom-0 z-50 w-1/4" src="{{ asset('storage/' . $member->photo) }}"
-            alt="{{ $member->name }}" data-aos="zoom-in-right">
-    @else
-        <img class="absolute right-52 bottom-0 z-50 w-1/4"
-            src="https://jkt48.com/profile/{{ strtolower(str_replace(' ', '_', $member->name)) }}.jpg"
-            alt="{{ $member->name }}" data-aos="zoom-in-right">
-    @endif
-    <section>
-        <h1>Nickname: {{ $member->nickname }}</h1>
-        <blockquote>"{{ $member->jikoshoukai }}"</blockquote>
-        <h1>Generation: {{ $member->generation }}</h1>
-        <h1>Status: {{ $member->status }}</h1>
-        <h1>Birthday: {{ $member->birthday }}</h1>
+
+    <section class="flex justify-around items-center h-max my-10">
+        <section class="flex justify-center items-center" data-aos="zoom-in-right" data-aos-delay="50">
+            @if ($member->photo)
+                <img class="z-50 w-30 rounded border-x-4 border-red-700 shadow-2xl shadow-red-700"
+                    src="{{ asset('storage/' . $member->photo) }}" alt="{{ $member->name }}" data-aos="zoom-in-right">
+            @else
+                <img class="z-50 w-30 rounded border-x-4 border-red-700 shadow-2xl shadow-red-700"
+                    src="https://jkt48.com/profile/{{ strtolower(str_replace(' ', '_', $member->name)) }}.jpg"
+                    alt="{{ $member->name }}" data-aos="zoom-in-right">
+            @endif
+        </section>
+        <section class="p-2 text-red-700 text-4xl flex flex-col justify-center gap-4">
+            <h1 class="px-3 py-2 border-y border-red-700 bg-red-50 rounded font-black" data-aos="fade-left"
+                data-aos-delay="50">{{ $member->name }}</h1>
+            <h1 class="px-3 py-2 border-y border-red-700 bg-red-50 rounded font-bold" data-aos="fade-left"
+                data-aos-delay="100">{{ $member->nickname }}</h1>
+            <h1 class="px-3 py-2 border-y border-red-700 bg-red-50 rounded font-bold" data-aos="fade-left"
+                data-aos-delay="150">Generation {{ $member->generation }}
+            </h1>
+            <h1 class="px-3 py-2 rounded font-bold @if ($member->status == 'Member') bg-green-50 text-green-700 ring-green-600/20 border-y border-green-700 @elseif ($member->status == 'Trainee') bg-yellow-50 text-yellow-700 ring-yellow-600/20 border-y border-yellow-700 @else bg-red-50 text-red-700 ring-red-600/20 border-y border-red-700 @endif"
+                data-aos="fade-left" data-aos-delay="200">
+                {{ $member->status }}</h1>
+            <h1 class="px-3 py-2 border-y border-red-700 bg-red-50 rounded font-bold" data-aos="fade-left"
+                data-aos-delay="250">{{ $member->birthday }}</h1>
+        </section>
     </section>
+    <blockquote class="border-l-4 text-5xl text-wrap text-red-700 border-red-700 p-2 bg-font" data-aos="zoom-in-left"
+        data-aos-delay="150">
+        "{{ $member->jikoshoukai }}"
+    </blockquote>
 @endsection
