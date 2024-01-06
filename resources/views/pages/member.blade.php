3b3bc4aa41ddf805037457c1d7faee8d5e9c363c
mfl4 <mhdfarhanl6267@gmail.com>
2024-01-06 19:49:30 +0700

    [PUSH] refac: show all data to member, members views & fix functionality of photo

--- resources/views/pages/member.blade.php
+++ resources/views/pages/member.blade.php
@@ -1,26 +1,44 @@
 @extends('layouts.main')
 
 @section('content')
-    <section class="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-        <h1 class="border-b border-red-700 p-4 text-2xl font-black tracking-tight text-red-700" data-aos="fade-down-left">
-            {{ $member->name }}</h1>
-        <h1 class="text-9xl bg-font opacity-80 text-center items-center -rotate-12 absolute right-52 bottom-52 animate-pulse"
-            data-aos="zoom-in-left">
-            {{ $member->name }}
-        </h1>
-        <img class="absolute right-52 bottom-0 z-50 w-1/3" src="{{ asset('images/fre.png') }} " alt="fre"
-            data-aos="zoom-in-right">
+    <section class="flex justify-between items-center">
+        <a href="{{ route('members') }}" class="font-black text-white bg-red-700 p-3 m-2 rounded text-center">Back
+            to Members</a>
         <section>
-            <img src="{{ $member->photo }}" alt="fre" class="w-1/3" data-aos="zoom-in-right">
-            <h1>Name: {{ $member->name }}</h1>
-            <h1>Nickname: {{ $member->nickname }}</h1>
-            <blockquote>"{{ $member->jikoshoukai }}"</blockquote>
-            <h1>Generation: {{ $member->generation }}</h1>
-            <h1>Status: {{ $member->status }}</h1>
-            <h1>Birthdate: {{ $member->date_of_birth }}</h1>
-            <h1>Zodiac: {{ $member->zodiac }}</h1>
-            <h1>Blood Type: {{ $member->blood_type }}</h1>
-            <h1>Height: {{ $member->height }}</h1>
+            @if ($member->id <= 1)
+                <span class="text-red-700 font-black p-3 m-2 rounded">No Previous Member</span>
+            @else
+                <a href="{{ route('member', $member->id - 1) }}"
+                    class="font-black text-white bg-red-700 p-3 m-2 rounded text-center">Previous Member</a>
+            @endif
+
+            @if ($member->id >= $member->count())
+                <span class="text-red-700 font-black p-3 m-2 rounded">No Next Member</span>
+            @else
+                <a href="{{ route('member', $member->id + 1) }}"
+                    class="font-black text-white bg-red-700 p-3 m-2 rounded text-center">Next Member</a>
+            @endif
         </section>
+    </section>
+    <h1 class="border-b border-red-700 p-4 text-2xl font-black tracking-tight text-red-700" data-aos="fade-down-left">
+        {{ $member->name }}</h1>
+    <h1 class="text-9xl bg-font opacity-80 text-center items-center -rotate-12 absolute right-52 bottom-52 animate-pulse"
+        data-aos="zoom-in-left">
+        {{ $member->name }}
+    </h1>
+    @if ($member->photo)
+        <img class="absolute right-52 bottom-0 z-50 w-1/4" src="{{ asset('storage/' . $member->photo) }}"
+            alt="{{ $member->name }}" data-aos="zoom-in-right">
+    @else
+        <img class="absolute right-52 bottom-0 z-50 w-1/4"
+            src="https://jkt48.com/profile/{{ strtolower(str_replace(' ', '_', $member->name)) }}.jpg"
+            alt="{{ $member->name }}" data-aos="zoom-in-right">
+    @endif
+    <section>
+        <h1>Nickname: {{ $member->nickname }}</h1>
+        <blockquote>"{{ $member->jikoshoukai }}"</blockquote>
+        <h1>Generation: {{ $member->generation }}</h1>
+        <h1>Status: {{ $member->status }}</h1>
+        <h1>Birthday: {{ $member->birthday }}</h1>
     </section>
 @endsection
--- resources/views/pages/members.blade.php
+++ resources/views/pages/members.blade.php
@@ -1,45 +1,47 @@
 @extends('layouts.main')
 
 @section('content')
-    <div class="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-        <h1 class="border-b border-red-700 p-4 text-2xl font-black tracking-tight text-red-700" data-aos="fade-down-left"
-            data-aos-delay="200" data-aos-duration="1000" data-aos-mirror="true" data-aos-once="false">
-            All
-            Members</h1>
+    <h1 class="border-b border-red-700 p-4 text-2xl font-black tracking-tight text-red-700" data-aos="fade-down-left"
+        data-aos-mirror="true" data-aos-once="false">
+        All
+        Members</h1>
 
-        <section class="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-            @foreach ($members as $member)
-                <section class="relative" data-aos="fade-down-right" data-aos-delay="{{ $member->generation }}0"
-                    data-aos-duration="500" data-aos-mirror="true" data-aos-once="false">
-                    <section
-                        class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-red-300 lg:aspect-none hover:opacity-75 lg:h-80">
-                        <img src="{{ asset('images/fre.png') }}" alt="{{ $member->name }}"
+    <section class="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+        @foreach ($members as $member)
+            <section class="relative" data-aos="fade-down-right" data-aos-delay="{{ $member->generation }}0"
+                data-aos-duration="500" data-aos-mirror="true" data-aos-once="false">
+                <section
+                    class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-red-300 border border-red-300 lg:aspect-none hover:opacity-75 lg:h-80">
+                    @if ($member->photo)
+                        <img src="{{ asset('storage/' . $member->photo) }}" alt="{{ $member->name }}"
                             class="h-full w-full object-cover object-center lg:h-full lg:w-full">
-                    </section>
-                    <section
-                        class="mt-2 flex justify-between items-center border border-l-8 border-red-300 rounded-r-lg p-4">
-                        <section class="space-y-2">
-                            <h2 class="text-lg font-bold text-red-700 ">
-                                <a href="{{ route('member', $member->id) }}">
-                                    <span aria-hidden="true" class="absolute inset-0"></span>
-                                    {{ $member->name }}
-                                </a>
-                            </h2>
-                            <section class="flex space-x-2">
-                                <a
-                                    class="inline-flex items-center rounded-md bg-red-50 px-2 py-1 text-xs font-medium text-red-700 ring-1 ring-inset ring-red-600/20">Gen
-                                    {{ $member->generation }}</a>
-                                <a
-                                    class="animate-bounce inline-flex items-center rounded-md px-2 py-1 text-xs font-medium ring-1 ring-inset @if ($member->status == 'Member') bg-green-50 text-green-700 ring-green-600/20 @elseif ($member->status == 'Trainee') bg-yellow-50 text-yellow-700 ring-yellow-600/20 @else bg-red-50 text-red-700 ring-red-600/20 @endif">{{ $member->status }}</a>
+                    @else
+                        <img src="https://jkt48.com/profile/{{ strtolower(str_replace(' ', '_', $member->name)) }}.jpg"
+                            alt="{{ $member->name }}" class="h-full w-full object-cover object-center lg:h-full lg:w-full">
+                    @endif
+                </section>
+                <section class="mt-2 flex justify-between items-center border border-l-8 border-red-300 rounded-r-lg p-4">
+                    <section class="space-y-2">
+                        <h2 class="text-lg font-bold text-red-700 ">
+                            <a href="{{ route('member', $member->id) }}">
+                                <span aria-hidden="true" class="absolute inset-0"></span>
+                                {{ $member->name }}
+                            </a>
+                        </h2>
+                        <section class="flex space-x-2">
+                            <a
+                                class="inline-flex items-center rounded-md bg-red-50 px-2 py-1 text-xs font-medium text-red-700 ring-1 ring-inset ring-red-600/20">Gen
+                                {{ $member->generation }}</a>
+                            <a
+                                class="animate-bounce inline-flex items-center rounded-md px-2 py-1 text-xs font-medium ring-1 ring-inset @if ($member->status == 'Member') bg-green-50 text-green-700 ring-green-600/20 @elseif ($member->status == 'Trainee') bg-yellow-50 text-yellow-700 ring-yellow-600/20 @else bg-red-50 text-red-700 ring-red-600/20 @endif">{{ $member->status }}</a>
 
-                            </section>
                         </section>
-                        <section class="hover:text-red-700 text-2xl">
-                            <i class="fa-regular fa-heart"></i>
-                        </section>
+                    </section>
+                    <section class="hover:text-red-700 text-2xl z-50 ">
+                        <i class="fa-regular fa-heart"></i>
                     </section>
                 </section>
-            @endforeach
-        </section>
-    </div>
+            </section>
+        @endforeach
+    </section>
 @endsection
